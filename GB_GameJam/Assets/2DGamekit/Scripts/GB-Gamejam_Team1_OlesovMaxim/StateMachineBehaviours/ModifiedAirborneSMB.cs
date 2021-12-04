66decad7489aa83a2da6c5bcfbb375eb745e5b8c
Windigo The Green <windigotp@gmail.com>
2021-12-04 21:32:21 +0800

    Base jetpack mechanic implemented

---
+++ GB_GameJam/Assets/2DGamekit/Scripts/GB-Gamejam_Team1_OlesovMaxim/StateMachineBehaviours/ModifiedAirborneSMB.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Gamekit2D;
+
+namespace GameJamTeamOne
+{
+    public class ModifiedAirborneSMB : AirborneSMB
+    {
+        #region Methods
+
+        public override void OnSLStateNoTransitionUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnSLStateNoTransitionUpdate(animator, stateInfo, layerIndex);
+
+            (m_MonoBehaviour as ModifiedPlayerCharacter).SetJetpacking(m_MonoBehaviour.CheckForJumpInput());
+        }
+
+        #endregion
+    }
+}
